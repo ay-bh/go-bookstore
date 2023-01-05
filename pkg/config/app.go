75a02fe7979934c607432112e03ae9f715dd8e17
Ayush Bhardwaj <96904351+ay-bh@users.noreply.github.com>
2023-01-05 17:06:35 +0530

    bug fixes

--- pkg/config/app.go
+++ pkg/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	d, err := gorm.Open("mysql", "root:mQD7hqvejvDq7qViVvmg@tcp(containers-us-west-105.railway.app:7030)/railway")
+	d, err := gorm.Open("mysql", "root:mQD7hqvejvDq7qViVvmg@tcp(containers-us-west-105.railway.app:7030)/railway?charset=utf8&parseTime=True&loc=Local")
 	if err != nil {
 		panic(err)
 	}
